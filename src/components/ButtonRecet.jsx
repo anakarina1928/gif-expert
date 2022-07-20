66e550c8b0419a7286e1801865ef79719c5c6695
anakarina1928 <anakarina.nak@gmail.com>
2022-07-20 09:33:47 -0500

    ruta absoluta de img

--- src/components/ButtonRecet.jsx
+++ src/components/ButtonRecet.jsx
@@ -8,7 +8,7 @@ const ButtonRecet = ({text,onClick} ) => {
     <nav  className="navbar navbar-light bg-light" onClick={onClick}>
       <div class="container" >
         
-          <img  src={"./risa.png"} alt="" width="50" height="50" className="d-inline-block align-text-top" />
+          <img  src={"https://raw.githubusercontent.com/anakarina1928/gif-expert/main/risa.png"} alt="" width="50" height="50" className="d-inline-block align-text-top" />
          <p className='reset'>{text}</p> 
        
       </div>
